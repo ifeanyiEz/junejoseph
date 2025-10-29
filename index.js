a239ffd6804eea44760d4cdc6928e25b90cf4982
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2025-10-29 20:02:43 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import methodOverride from "method-override";
+
+const app = express();
+const port = 5000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Your blog server is running on port ${port}.`);
+});
