7f6e5ceeb6536489c60cf8459549eddfda24ad5b
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2025-11-08 13:01:07 +0100

    Include the login route

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Your blog server is running on port ${port}.`);
 });
