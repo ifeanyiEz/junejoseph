e467f052a287c16ab3b555d62f7dea0a7c9aad06
root <root@EZUGWORIE.>
2025-11-23 01:25:41 +0100

    Add dynamic responsiveness to portal sidebar and content area

---
+++ public/js/portal-sidebar.js
@@ -0,0 +1,50 @@
+
+const sidebar = document.getElementById("mySidebar");
+const content = document.getElementById("portalMain");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+function getSidebarPercent() {
+    const width = sidebar.offsetWidth;
+    return (width / window.innerWidth) * 100;
+}
+
+function openSidebar() {
+    // Let sidebar auto-size (px or max-content or any size)
+    sidebar.style.width = "max-content";
+
+    if (window.innerWidth >= 1201) {
+        // Wait for rendering to update
+        requestAnimationFrame(() => {
+            const percent = getSidebarPercent();
+
+            content.style.margin = `0.5% 0.5% 0.5% ${percent + 0.5}%`;
+            content.style.width = `calc(${99 - percent}% )`;
+        });
+    }
+}
+
+function closeSidebar() {
+    sidebar.style.width = "0";
+
+    if (window.innerWidth >= 1201) {
+        content.style.margin = "0.5% 0.5%";
+        content.style.width = "99%";
+    }
+}
+
+openBtn.addEventListener("click", openSidebar);
+closeBtn.addEventListener("click", closeSidebar);
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 1201) {
+        content.style.margin = "1% 1%";
+        content.style.width = "98%";
+    } else {
+        if (sidebar.offsetWidth > 0) {
+            openSidebar(); // recalc percent for new viewport width
+        } else {
+            closeSidebar();
+        }
+    }
+});
