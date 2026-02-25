46aff42f87ec490da82031588dcd7579e43d5bc9
root <root@EZUGWORIE.>
2026-02-25 05:50:26 +0100

    Add dynamic time-based updates

---
+++ public/js/time-update.js
@@ -0,0 +1,48 @@
+
+(function () {
+    
+    const dataElement = document.getElementById("dashboard-data");
+    const { user, messages } = JSON.parse(dataElement.textContent);
+
+    const greetingsText = document.getElementById("greetings");
+    const adviceText = document.getElementById("dash-message");
+    const timeIcon = document.getElementById("time-icon");
+
+    let currentTimeWindow = null;
+    
+    function getTimeWindow(hour) {
+        if (hour >= 0 && hour < 4) return "midnight";
+        if (hour >= 4 && hour < 8) return "earlyMorning";
+        if (hour >= 8 && hour < 12) return "morning";
+        if (hour >= 12 && hour < 16) return "afternoon";
+        if (hour >= 16 && hour < 20) return "evening";
+        return "night";
+    }
+
+
+    function updateDashboard() {
+        const hour = new Date().getHours();
+        const tWindow = getTimeWindow(hour);
+
+        if (tWindow === currentTimeWindow) return;
+
+        currentTimeWindow = tWindow;
+
+        const greeting = getGreeting(hour);
+        const gender = user.gender || "neutral";
+
+        const advice = messages[tWindow][gender];
+
+        const selectedIcon = iconMap[greeting] || {
+            icon: "bi-cloud",
+            cssClass: ""
+        };
+
+        greetingsText.textContent = `${greeting}, ${user.firstName}.`;
+        adviceText.textContent = advice;
+        timeIcon.className = `bi ${selectedIcon.icon} right-text ${selectedIcon.cssClass}`;
+    }
+
+    updateDashboard();
+    setInterval(updateDashboard, 300000);
+})();
