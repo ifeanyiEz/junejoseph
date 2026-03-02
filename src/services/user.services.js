6162b84209b0c901317c062d41ca347e7e500889
root <root@EZUGWORIE.>
2026-03-02 03:28:11 +0100

    Update user services

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,6 +1,8 @@
-import { hashPassword } from '../utils/password_hash.util.js';
-import { slugGen } from '../utils/slug.util.js';
-import { createUser } from '../models/user.model.js';
+import { hashPassword } from '../utils/password_hash.utils.js';
+import { slugGen } from '../utils/slug.utils.js';
+import { createUser } from '../models/user.models.js';
+import { comparePassword } from '../utils/password_hash.utils.js';
+import { getUserByEmail } from '../models/user.models.js';
 
 
 export async function registerUser(userData) {
@@ -12,4 +14,25 @@ export async function registerUser(userData) {
         user_id,
         password: hashedPassword
     });
+}
+
+export async function loginUser(email, password) {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+        return null;
+    }
+
+    const passwordMatch = await comparePassword(password, user.password);
+
+    if (!passwordMatch) {
+        return null;
+    }
+
+    return {
+        user_id: user.user_id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email
+    };
 }
