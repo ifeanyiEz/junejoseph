9a3443bf0b93d3ff1c1cc3e7cb342be4266acb13
root <root@EZUGWORIE.>
2026-03-07 03:06:13 +0100

    Emphasize conncetion string

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,7 @@ const config = {
     port: process.env.PORT || 5000,
 
     db: {
+        connectionString: process.env.DATABASE_URL,
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         password: process.env.DB_PASSWORD,
