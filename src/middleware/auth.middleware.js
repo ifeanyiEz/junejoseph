878669e91a35818c7071cd084a8fe795f1b7d40d
root <root@EZUGWORIE.>
2026-03-08 08:13:35 +0100

    Middleware sub-directory

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,9 @@
+
+export const requireAuth = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
+
